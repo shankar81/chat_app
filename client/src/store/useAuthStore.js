bb4c1daf318d4aa83d98d588d4d7ac7edf5c3615
shankar sawant <shankarsawant81@gmail.com>
2025-10-21 21:45:56 +0530

    integrate axios interceptor and zustand

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import { axiosInstance } from "../utils/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isRegister: false,
+  isLogin: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      set({ authUser: null });
+      console.log("Error in checking auth", error);
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
--- server/src/index.js
+++ server/src/index.js
@@ -1,41 +1,40 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import { connectDB } from './utils/db.js';
+import { connectDB } from "./utils/db.js";
 
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 
-import authRoute from './routes/auth.route.js'
-import messageRoute from './routes/message.route.js'
+import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 
-const app = express()
+const app = express();
 
-dotenv.config()
+dotenv.config();
 
-
-const PORT = process.env.PORT
-app.use(cors())
-app.use(express.json())
+const PORT = process.env.PORT;
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 //cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-app.use('/api/auth',authRoute)
-app.use('/api/auth',messageRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/auth", messageRoute);
 
-app.listen(PORT,()=>{
-   console.log(`app is listining on ${PORT}`)
-   connectDB()
-})
+app.listen(PORT, () => {
+  console.log(`app is listining on ${PORT}`);
+  connectDB();
+});
