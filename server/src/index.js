e7c732a6c313e0c6dc996147b18ca1b7b3e6ba60
shankar sawant <shankarsawant81@gmail.com>
2025-10-24 18:35:07 +0530

    fix deployment bug

--- server/src/index.js
+++ server/src/index.js
@@ -32,10 +32,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "client", "dist")));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
